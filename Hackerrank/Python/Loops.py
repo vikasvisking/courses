92263fabc44e84522918d5c86818bf25bcf7c568
Vikas Passi <vikasvisking007@gmail.com>
2019-01-10 21:58:03 +0530

    add loops file

---
+++ Hackerrank/Python/Loops.py
@@ -0,0 +1,25 @@
+# Task
+# Read an integer . For all non-negative integers , print . See the sample for details.
+
+ # solutions:
+
+
+ if __name__ == '__main__':
+    n = int(input())
+
+
+for i in range(0 , n ):
+    i = i**2
+    print(i)
+
+
+
+# OR
+
+
+if __name__ == '__main__':
+    n = int(input())
+
+
+for i in range(0 , n ):
+    print (pow(i,2))
