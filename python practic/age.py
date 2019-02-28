3e6ab68f597fa111a64415beecfbfc19685e3006
Vikas Passi <vikasvisking007@gmail.com>
2019-02-28 10:50:41 +0530

    age program

---
+++ python practic/age.py
@@ -0,0 +1,5 @@
+#Create a program that asks the user to enter their name and their age. Print out a message addressed to them that tells them the year that they will turn 100 years old.
+
+
+age = int(input("please enter your age: "))
+print("You will turn 100 years old in " + str((2019 - age) + 100))
