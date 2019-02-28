e0c720452a207fe6b3aa4d3e39381393ad219e9e
Vikas Passi <vikasvisking007@gmail.com>
2019-02-28 10:53:56 +0530

    addition in it

--- python practic/age.py
+++ python practic/age.py
@@ -1,5 +1,5 @@
 #Create a program that asks the user to enter their name and their age. Print out a message addressed to them that tells them the year that they will turn 100 years old.
 
-
+name = raw_input("please enter your name: ")
 age = int(input("please enter your age: "))
-print("You will turn 100 years old in " + str((2019 - age) + 100))
+print( name +" will turn 100 years old in " + str((2019 - age) + 100))
